fc076eb15e5d8f56b6db4d9b97c0afbb25684841
Tim Lu <hl4851@nyu.edu>
2023-10-22 16:49:33 -0400

    Add common CLI tool settings

--- shell/settings.sh
+++ shell/settings.sh
@@ -1,3 +1,15 @@
 export PATH=$HOME/.local/bin:$PATH
 
 export EDITOR=vim
+
+export BAT_THEME='Monokai Extended'
+
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border=rounded --info inline'
+export FZF_DEFAULT_COMMAND='fd --hidden --follow --type f --exclude .git'
+_fzf_compgen_path() {
+    fd --hidden --follow --exclude ".git" ."$1"
+}
+_fzf_compgen_dir() {
+    fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
--- zsh/settings.sh
+++ zsh/settings.sh
@@ -3,3 +3,13 @@ setopt prompt_subst
 
 # Load color variables to make it easier to color things
 autoload -U colors && colors
+
+# Initialize editing command line
+autoload -U edit-command-line && zle -N edit-command-line
+
+# Enable interactive comments (# on the command line)
+setopt interactivecomments
+
+# Use vim style line editing in zsh
+# - Undo
+bindkey -a 'u' undo
